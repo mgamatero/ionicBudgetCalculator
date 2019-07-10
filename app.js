f34f1d0fe6236ae2924c41cd8af2c010842d191a
Mike Gamatero <mgamatero@gmail.com>
2019-07-10 16:38:01 -0700

    native alert for missing input added

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
-const reasonInput = document.querySelector('#input-reason');
-const amountInput = document.querySelector('#input-amount');
+const reasonInput = document.querySelector("#input-reason");
+const amountInput = document.querySelector("#input-amount");
 
-const cancelBtn= document.querySelector("#btn-cancel");
-const confirmBtn= document.querySelector("#btn-add");
+const cancelBtn = document.querySelector("#btn-cancel");
+const confirmBtn = document.querySelector("#btn-add");
 
 const expensesList = document.querySelector("#expenses-list");
-const totalExpensesOutput = document.querySelector("#total-expenses")
+const totalExpensesOutput = document.querySelector("#total-expenses");
+const alertCtrl = document.querySelector('ion-alert-controller');
 
 let totalExpenses = 0;
 
+clear = () => {
+  reasonInput.value = "";
+  amountInput.value = "";
+};
 
-clear = () =>{
-    reasonInput.value='';
-    amountInput.value='';
-}
+confirmBtn.addEventListener("click", function() {
+  const enteredReason = reasonInput.value;
+  const enteredAmount = amountInput.value;
 
-confirmBtn.addEventListener('click',function(){
-    const enteredReason = reasonInput.value;
-    const enteredAmount = amountInput.value;
-    
-    if(enteredReason.trim()<=0||enteredAmount<=0||enteredAmount.trim().length<=0){
-        return
-    }
-    console.log(enteredReason+' '+enteredAmount)
-    const newItem = document.createElement('ion-item');
-    newItem.textContent = enteredReason+': $'+enteredAmount;
-    expensesList.appendChild(newItem)
-    totalExpenses += +enteredAmount;
-    totalExpensesOutput.textContent = totalExpenses;
-    clear();
-})
+  if (
+    enteredReason.trim() <= 0 ||
+    enteredAmount <= 0 ||
+    enteredAmount.trim().length <= 0
+  ) {
+    alertCtrl.create({
+      message: 'Please enter valid reason and amount!',
+      header: 'Invalid inputs!',
+      buttons: ['Okay']
+    }).then(alertElement => {
+        alertElement.present();
+    });
+    return;
+  }
+  
+  const newItem = document.createElement("ion-item");
+  newItem.textContent = enteredReason + ": $" + enteredAmount;
+  expensesList.appendChild(newItem);
+  totalExpenses += +enteredAmount;
+  totalExpensesOutput.textContent = totalExpenses;
+  clear();
+});
 
-cancelBtn.addEventListener('click',function(){
-    clear();
-    
-})
+cancelBtn.addEventListener("click", function() {
+  clear();
+});
